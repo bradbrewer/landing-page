7e93b352193b8b62e9edfbcd62a287da1535a065
bradbrewer <bradbrewermail@gmail.com>
2023-07-28 13:50:59 -0500

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      <img className="w-8 h-8" src="i_logo.jpg"></img>
+      <img className="w-8 h-8" src="ui/i_logo.jpg"></img>
       </Link>
   )
 }
